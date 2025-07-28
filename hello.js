4d0fea0b22a6c243491387e31a64732831794d14
mary <mary.a.hill03@gmail.com>
2025-07-28 16:27:47 +0930

    edited the wrong hello file.  corrected

--- hello.js
+++ hello.js
@@ -1,7 +1,4 @@
-const hello = require("./hello");
-describe("My hello" , () => {
-    test("works" , () => {
-        expect(hello.hello()).toEqual("Hello World from Australia");
-    });
-});
+const hello = () => "Hello World from Australia";
+console.log(hello());
+exports.hello = hello;
 
